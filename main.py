1983a0ca6c8adef9991298915d22b18603def5e5
AnnaLipenberga <124579305+AnnaLipenberga@users.noreply.github.com>
2023-03-17 21:10:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,3 +26,39 @@ def main():
 if __name__ == "__main__":
     main()
 
+
+    
+    
+    
+    import heapq
+
+def parallel_processing(n, data):
+    output = []
+    thread = n*[0]
+    heap = [(0, i) for i in range(n)]
+    heapq.heapify(heap)
+
+    for job_exec_time in data:
+        
+        _, thread = heapq.heappop(heap)
+
+        output.append((thread, thread_busy_times[thread]))
+        thread[thread] += job_exec_time
+
+        
+        heapq.heappush(heap, (thread[thread], thread))
+
+    return output
+
+
+def main():
+    n, _ = map(int, input().split())
+    data = list(map(int, input().split()))
+
+    result = parallel_processing(n, data)
+    for thread, start_time in result:
+        print(thread, start_time)
+
+
+if __name__ == "__main__":
+    main()
