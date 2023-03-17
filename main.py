ab00e66722ad894ce05cd4c1c4ae628f16e3ace7
AnnaLipenberga <124579305+AnnaLipenberga@users.noreply.github.com>
2023-03-17 20:46:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,93 +1,71 @@
 # python3
 
-def swapping(data, i, res):
-    left_child = 1 + (2*i)
-    right_child = 2 + (2*i)
-    n = len(data)
-    
-    if left_child >= n:
-        return
-    x = left_child
-    if right_child < n and data[left_child] > data[right_child]:
-        x = right_child
-        
-    if data[i] > data[x]:
-        res.append((i, x))
-        data[i], data[x] = data[x], data[i]
-        swapping(data, x, res)
-        
-        
 def build_heap(data):
     swaps = []
-    n = len(data)
-    for i in range(int((n-2)/2), -1, -1):
-        swapping(data, i, swaps)
-        
-    return swaps
-        
-
-def parallel_processing(n, m, data):
-    output = []
-    heap = data[:n]
-    data_idx = n
-    processing_threads = list(range(n))
-    start_times = [0] * n
-    
-    if data_idx <= m:
-        for i in processing_threads:
-            if data_idx < m:
-                start_times[i] = data[data_idx]
-                output.append((i, start_times[i]))
-                heap[i] = start_times[i] + data[data_idx]
-                data_idx += 1
-            
-        swaps = build_heap(heap)
-        for i, j in swaps:
-            processing_threads[i], processing_threads[j] = processing_threads[j], processing_threads[i]
-            
-        for i in range(n):
-            if data_idx < m:
-                start_times[i] = heap[i] - data[data_idx]
-                output.append((processing_threads[i], start_times[i]))
-                heap[i] = start_times[i] + data[data_idx]
-                data_idx += 1
+    l = len(data)
+    fr = int((l-2)/2)   
+    def ch(i):
+        if(2*i+2>=l):
+            min=2*i+1
+        else:
+            if(data[2*i+1]>data[2*i+2]):
+                min = 2*i+2
             else:
-                output.append((processing_threads[i], start_times[i]))
-    
-    return output
+                min = 2*i+1
+        if(data[min]<data[i]):
+            swaps.append([i, min])
+            data[min], data[i] = data[i], data[min]
+            if(min*2+1<=l-1):
+                ch(min) 
+        else:
+            return
+    while(fr>=0):   
+        ch(fr)
+        fr-=1
+
+    return swaps
 
 
 def main():
-    # create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n, m = map(int, input().split())
-    source = input()
-    if "I" in source:
-        n = int(input())
-        data = list(map(int, input().split()))
-    elif "F" in source:
-        source2 = input()
-        if "a" in source2:
-            return()
-        with open ("tests/"+ source2, encoding="utf-8") as fails:
-            n = int (fails.readline())
-            data = list(map(int, fails.readline().split()))
-    else:
-        return()
-
-   
-
-    # pass the input data to the function
-    result = parallel_processing(n, m, data)
     
-    # print out the results, each pair in its own line
-    for pair in result:
-        print(pair[0], pair[1])
+    # TODO : add input and corresponding checks
+    # add another input for I or F 
+    # first two tests are from keyboard, third test is from a file
+    let = input()[0]
+    if let == 'F' :
+        text = input()
+        if 'a' in text:
+            return
+        text = "tests/" + text
+        with open(text) as file:
+            n = int(file.readline())
+            data = [int(i) for i in file.readline().split(' ')]          
+    elif let == 'I' :
+        n = int(input())
+        data = [int(i) for i in input().split(' ')]
+    else: 
+        return
+
+    # input from keyboard
+    # n = int(input())
+    # data = list(map(int, input().split()))
+
+    # checks if lenght of data is the same as the said lenght
+    assert len(data) == n
+
+    # calls function to assess the data 
+    # and give back all swaps
+    swaps = build_heap(data)
+
+    # TODO: output how many swaps were made, 
+    # this number should be less than 4n (less than 4*len(data))
+
+
+    # output all swaps
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
 
 
 if __name__ == "__main__":
     main()
-
