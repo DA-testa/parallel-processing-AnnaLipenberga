ca80b4a1c0a01b5376589902cd81aa9f8fd58cad
AnnaLipenberga <124579305+AnnaLipenberga@users.noreply.github.com>
2023-03-17 21:11:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,5 @@
 # python3
-
-def parallel_processing(n, data):
-    output = []
-    thread_busy_times = [0] * n
-
-    for job_exec_time in data:
-        first_to_finish = min(thread_busy_times)
-        thread = thread_busy_times.index(first_to_finish)
-
-        output.append((thread, first_to_finish))
-        thread_busy_times[thread] += job_exec_time
-
-    return output
-
-
-def main():
-    n, _ = map(int, input().split(' '))
-    data = list(map(int, input().split(' ')))
-
-    result = parallel_processing(n, data)
-    for thread, start_time in result:
-        print(thread, start_time)
-
-
-if __name__ == "__main__":
-    main()
-
-
-    
-    
+ 
     
     import heapq
 
